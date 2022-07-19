9a740c7c7f1a2d8e82e99843914bb97ae86ab80f
Said Mohamed <said.mohamed@fewidev.com>
2022-07-19 14:50:49 -0500

    test

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # two decorators, same function 
-@app.route('/')
+@app.route('/') 
 @app.route('/index.html')
 def index():
     return render_template('index.html', the_title='Tiger Home Page')
